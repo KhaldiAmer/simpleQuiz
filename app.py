7eb6afafafc725baa626832509189e76bbd63d62
Amer Khaldi <khaldi.ameur1995@gmail.com>
2018-07-14 13:20:00 +0100

    Add the answer when user gets the correct answer.

--- app.py
+++ app.py
@@ -2,16 +2,38 @@
 # -*- coding: utf-8 -*-
 
 # paragraphs for levels 
-easy = "word word word [missing word 1] word word word word word word [missing word 2], word word [missing word 3], word word word word [missing word 4]"
-medium = "word word word [missing word 1] word word word word word word [missing word 2], word word [missing word 3], word word word word [missing word 4]"
-hard = "word word word [missing word 1] word word word word word word [missing word 2], word word [missing word 3], word word word word [missing word 4]"
+easy = """
+Eminem - Lose yourself
+His palms are [TO_BE_FILLED_1], knees weak, arms are [TO_BE_FILLED_2]
+There's vomit on his sweater [TO_BE_FILLED_3], mom's spaghetti 
+He's nervous, but on the surface he looks calm and ready 
+To drop bombs, but he keeps on forgettin' 
+What he wrote down, the whole [TO_BE_FILLED_4] goes so loud 
+He opens his mouth, but the words won't come out """
+
+medium = """
+Eminem - Not afraid.
+You can try and read my lyrics off of this [TO_BE_FILLED_1] before I lay 'em
+But you won't take the [TO_BE_FILLED_2] out these words before I say 'em
+'Cause ain't no way I'ma let you stop me from causin' mayhem
+When I say I'ma do [TO_BE_FILLED_3], I [TO_BE_FILLED_4] it """
+
+hard = """
+Eminem - rap god
+Look, I was gonna go easy on you and not to hurt your [TO_BE_FILLED_1]
+But I'm only going to get this one [TO_BE_FILLED_2]
+Something's wrong, I can feel it (Six minutes, Slim Shady, you're on)
+Just a feeling I've got, like something's about to [TO_BE_FILLED_3], but I don't know what
+If that means, what I think it means, we're in trouble, big trouble,
+And if he is as bananas as you say, I'm not taking any chances
+You were just what the [TO_BE_FILLED_4] ordered """
 # answers for levels 
-easy_answers = ["word1","word2","word3","word4"]
-medium_answers = ["word1","word2","word3","word4"]
-hard_answers = ["word1","word2","word3","word4"]
+easy_answers = ["sweaty","heavy","already","crowd"]
+medium_answers = ["paper","sting","something","do"]
+hard_answers = ["feelings","chance","happen","doctor"]
 
 def select_level():
-	print("Hello to Amer's quiz")
+	print("Hello to Eminem's words quiz")
 	level = raw_input("Please select a level: easy or medium or hard \r\n")
 	if level == "easy": 
 		return go(easy,easy_answers)
@@ -29,6 +51,8 @@ def go(text, answers):
 		answer = raw_input("What is {}?\r\n".format(x+1))
 		if answer == answers[x]:
 			print("Awesome! next one!")
+			text = text.replace("[TO_BE_FILLED_{}]".format(x+1),answers[x])
+			print(text)
 		else:
 			print("Wrong answer")
 			continue
