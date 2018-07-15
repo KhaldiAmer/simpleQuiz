0d2942b3a725551f50e60c7c211f9774df537aa2
Amer Khaldi <khaldi.ameur1995@gmail.com>
2018-07-15 10:58:39 +0100

    Update project.

--- app.py
+++ app.py
@@ -1,65 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# paragraphs for levels 
-easy = """
-Eminem - Lose yourself
-His palms are [TO_BE_FILLED_1], knees weak, arms are [TO_BE_FILLED_2]
-There's vomit on his sweater [TO_BE_FILLED_3], mom's spaghetti 
-He's nervous, but on the surface he looks calm and ready 
-To drop bombs, but he keeps on forgettin' 
-What he wrote down, the whole [TO_BE_FILLED_4] goes so loud 
-He opens his mouth, but the words won't come out """
+from constants import *
 
-medium = """
-Eminem - Not afraid.
-You can try and read my lyrics off of this [TO_BE_FILLED_1] before I lay 'em
-But you won't take the [TO_BE_FILLED_2] out these words before I say 'em
-'Cause ain't no way I'ma let you stop me from causin' mayhem
-When I say I'ma do [TO_BE_FILLED_3], I [TO_BE_FILLED_4] it """
-
-hard = """
-Eminem - rap god
-Look, I was gonna go easy on you and not to hurt your [TO_BE_FILLED_1]
-But I'm only going to get this one [TO_BE_FILLED_2]
-Something's wrong, I can feel it (Six minutes, Slim Shady, you're on)
-Just a feeling I've got, like something's about to [TO_BE_FILLED_3], but I don't know what
-If that means, what I think it means, we're in trouble, big trouble,
-And if he is as bananas as you say, I'm not taking any chances
-You were just what the [TO_BE_FILLED_4] ordered """
-# answers for levels 
-easy_answers = ["sweaty","heavy","already","crowd"]
-medium_answers = ["paper","sting","something","do"]
-hard_answers = ["feelings","chance","happen","doctor"]
-
-def select_level():
+def startQuiz():
+	""" starts the quiz and directs the user to the specified level. """
 	print("Hello to Eminem's words quiz")
 	level = raw_input("Please select a level: easy or medium or hard \r\n")
 	if level == "easy": 
-		return go(easy,easy_answers)
+		return redirectTo(easy,easy_answers)
 	elif level == "medium":
-		return go(medium,medium_answers)
+		return redirectTo(medium,medium_answers)
 	elif level == "hard":
-		return go(hard,hard_answers)
-	print "You should choose again"
-	return select_level()
-# Starts the quiz, checks answers, limits users input
-def go(text, answers):
-	print(text)
+		return redirectTo(hard,hard_answers)
+	else:
+		print("You should choose again")
+		return startQuiz()
+
+
+def redirectTo(level, answers):
+	""" Loops through the quiz answers to get the unfilled words. """
+	print(level)
 	x = 0
 	while x < len(answers):
-		answer = raw_input("What is {}?\r\n".format(x+1))
+		answer = raw_input("What is [TO_BE_FILLED_{}]?\r\n".format(x+1))
 		if answer == answers[x]:
 			print("Awesome! next one!")
-			text = text.replace("[TO_BE_FILLED_{}]".format(x+1),answers[x])
-			print(text)
+			level = level.replace("[TO_BE_FILLED_{}]".format(x+1),answers[x])
+			print(level)
 		else:
 			print("Wrong answer")
 			continue
 		x += 1
 	retry = raw_input("You got it all right! want to try again? y or n\r\n")
 	if retry == "y":
-		select_level()
+		startQuiz()
 	exit()
-select_level()
+startQuiz()
 
